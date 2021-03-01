5a922c54e937ef93ae9571a95565d69f1385fe44
noorqidam <noorqidam@gmail.com>
2021-03-01 22:24:50 +0700

    add API Update Profile

--- routes/users.js
+++ routes/users.js
@@ -4,5 +4,6 @@ const userHandler = require("./handler/users");
 
 router.post("/register", userHandler.register);
 router.post("/login", userHandler.login);
+router.put("/:id", userHandler.update);
 
 module.exports = router;
